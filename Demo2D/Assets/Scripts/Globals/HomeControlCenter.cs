36f04525caa8c85566940582865a23dbb5e602dc
Nicolas Zuasti <nicozuasti@gmail.com>
2014-09-29 14:43:24 -0300

    carpetas

---
+++ Demo2D/Assets/Scripts/Globals/HomeControlCenter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeControlCenter : MonoBehaviour {
+
+	private GameObject guimanager;
+
+	// Use this for initialization
+	void Start () {
+		guimanager = GameObject.Find ("guiManager");
+		GameControl gc = (GameControl)guimanager.GetComponent<GameControl>();
+		gc.DrawHomeMainMenu();
+
+	}
+}
